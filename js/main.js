f0f0e1bbc2818fc58c13889d4d40eaec032885cc
Artyom Soloveyov <soloviy.shpak16@gmail.com>
2024-04-18 18:47:45 +0300

    Added testimonials slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-Fancybox.bind('[data-fancybox]', {
-  // Your custom options
+// init gallery
+Fancybox.bind('[data-fancybox]', {});
+
+// testimonials slider
+var swiper = new Swiper('.testimonials__slider', {
+  navigation: {
+    nextEl: '.testimonials__slider-next',
+    prevEl: '.testimonial__slide-prev',
+  },
+  effect: 'cards',
+  grabCursor: true,
 });
